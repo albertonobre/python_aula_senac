48a5986ee794d1f57dbb606efc50d6250856efa4
albertonobre <alberto_nobre@yahoo.com.br>
2023-11-29 11:56:39 -0300

    conhecendo variaveis

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+# isso é um comentário...
+"""
+Esse é um 
+comentário com varias 
+linhas
+"""
+
+print ("Hello World")
---
+++ variaveis.py
@@ -0,0 +1,19 @@
+"""
+str (string) = "textos"
+int (inteiro) = numero inteiro ex: 10
+float = 10.5
+boolean = true
+"""
+
+#nomes para variavel
+nome = "Alberto"
+print(nome)
+#mostrando o tipo
+print(type(nome))
+#função lenght (espaços tbm conta)
+print(len(nome))
+#função Uper Case
+print(nome.upper())
+
+for x in nome:
+    print(x)
